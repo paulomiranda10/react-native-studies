695940a0e9eaa5788acc5acb9170032c9f14b135
paulomiranda10 <paulomiranda10@gmail.com>
2020-08-22 15:47:37 -0300

    first commit

---
+++ react_native_ListGitHubReps/src/config/ReactotronConfig.js
@@ -0,0 +1,22 @@
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({
+    host: 'localhost',
+    port: 9090,
+  })
+    .useReactNative()
+    .connect();
+
+  console.tron = tron;
+
+  tron.clear();
+}
+
+/**
+ * {
+    host: 'localhost',
+    port: 9090,
+  }
+ *
+ */
---
+++ react_native_tddjest/abre_studio.sh
@@ -0,0 +1,2 @@
+sudo chmod 777 /dev/kvm 
+/home/paulo/dev/android-studio/bin/studio.sh
